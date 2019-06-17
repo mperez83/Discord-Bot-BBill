553bae3dfe0482dba4892b1d7d875d58faeaef07
Michael Perez <mperez980089@yahoo.com>
2019-06-16 19:07:49 -0700

    Added shibe rarities

--- javascript/commands/shibe.js
+++ javascript/commands/shibe.js
@@ -2,7 +2,10 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const utilitiesModule = require('../utilities');
 
+
+
 module.exports.run = async (bot, message, args) => {
+
     utilitiesModule.readJSONFile("./data/shibeData.json", function (shibeDataJson) {
 
         let shibes = [];
@@ -14,6 +17,9 @@ module.exports.run = async (bot, message, args) => {
 
         if (!shibeDataJson[shibes[randomIndex]]) shibeDataJson[shibes[randomIndex]] = { amount: 0 };
         shibeDataJson[shibes[randomIndex]].amount++;
+
+        if (!shibeDataJson[shibes[randomIndex]].rarity) shibeDataJson[shibes[randomIndex]].rarity = "Not set yet";
+
         fs.writeFileSync("./data/shibeData.json", JSON.stringify(shibeDataJson), function(err) {if (err) return err;});
 
         let stats = fs.statSync("./shibes/" + shibes[randomIndex]);
@@ -29,12 +35,34 @@ module.exports.run = async (bot, message, args) => {
         if (shibes[randomIndex] == "komugi shibe 1.jpg") {
             //message.channel.send(`**Holy shit, you unboxed '${shibes[randomIndex]}'!! There was a 1/${shibes.length} chance of that happening!**\n${sizeString}`);
             newEmbed.addField(`Holy shit, a  ${shibes[randomIndex]}!!`, `There was a 1/${shibes.length} chance of that happening!`);
+            newEmbed.setColor("#ff0000");
         }
         else {
+            newEmbed.addField(`Rarity`, `${shibeDataJson[shibes[randomIndex]].rarity}`);
             newEmbed.addField(`${shibes[randomIndex]}`, `Amount unboxed: ${shibeDataJson[shibes[randomIndex]].amount}`);
             newEmbed.addField(`Size`, `${fileSize}mb`);
-            //newEmbed.attachFile("./shibes/" + shibes[randomIndex]);
-            //message.channel.send(`**${shibes[randomIndex]}** (Amount unboxed: **${shibeDataJson[shibes[randomIndex]].amount}**)\n${sizeString}`);
+
+            switch(shibeDataJson[shibes[randomIndex]].rarity) {
+                case "Common":
+                    newEmbed.setColor("#bcbcbc");
+                    break;
+
+                case "Uncommon":
+                    newEmbed.setColor("#5bff7f");
+                    break;
+                
+                case "Rare":
+                    newEmbed.setColor("#5468ff");
+                    break;
+
+                case "Epic":
+                    newEmbed.setColor("#c62bff");
+                    break;
+                
+                case "Legendary":
+                    newEmbed.setColor("#f2ff00");
+                    break;
+            }
         }
 
         message.channel.send(newEmbed);
@@ -42,6 +70,41 @@ module.exports.run = async (bot, message, args) => {
 
         utilitiesModule.incrementUserDataValue(message.author, "shibeCalls", 1);
 
+        const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
+        collector.on('collect', message => {
+
+            msg = message.content.toLowerCase();
+            if (msg == "common" || msg == "uncommon" || msg == "rare" || msg == "epic" || msg == "legendary") {
+
+                switch (msg) {
+                    case "common":
+                        shibeDataJson[shibes[randomIndex]].rarity = "Common";
+                        break;
+
+                    case "uncommon":
+                        shibeDataJson[shibes[randomIndex]].rarity = "Uncommon";
+                        break;
+                    
+                    case "rare":
+                        shibeDataJson[shibes[randomIndex]].rarity = "Rare";
+                        break;
+
+                    case "epic":
+                        shibeDataJson[shibes[randomIndex]].rarity = "Epic";
+                        break;
+                    
+                    case "legendary":
+                        shibeDataJson[shibes[randomIndex]].rarity = "Legendary";
+                        break;
+                }
+
+                fs.writeFileSync("./data/shibeData.json", JSON.stringify(shibeDataJson), function(err) {if (err) return err;});
+                message.react("✅");
+                collector.stop();
+
+            }
+
+        });
     });
 }
 
