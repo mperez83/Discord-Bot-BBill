a310ed7a1cc514561ce1939c35b36ca41256ff69
Michael Perez <mperez980089@yahoo.com>
2019-06-21 23:29:47 -0700

    Cleaned up urban, added pride command

--- javascript/commands/urban.js
+++ javascript/commands/urban.js
@@ -3,36 +3,41 @@ const urban = require("urban-dictionary");
 
 module.exports.run = async (bot, message, args) => {
     args = args.join(" ");
+
+    //Random urban call
     if (args.length == 0) {
         urban.random((error, entry) => {
             if (error) {
-                message.channel.send("Something went wrong, " + utilitiesModule.getRandomNameInsult());
+                message.channel.send(`Something went wrong, ${utilitiesModule.getRandomNameInsult}`);
             }
             else {
-                text = "**Word**: " + entry.word +
-                    "\n\n**Definition**: *" + entry.definition + "*" +
-                    "\n\n**Example**: " + entry.example +
-                    "\n\n<" + entry.permalink + ">";
-                if (text.length >= 2000) message.channel.send("That urban dictionary article is too long!\n<" + entry.permalink + ">");
+                text = `**Word**: ${entry.word} (<${entry.permalink}>)
+                \n**Definition**: *${entry.definition}*
+                \n**Example**: ${entry.example}`;
+
+                if (text.length >= 2000) message.channel.send(`The urban dictionary article for '${entry.word}' is too long!\n<${entry.permalink}>`);
                 else message.channel.send(text);
             }
         });
     }
+
+    //Specific urban call
     else {
         urban.term(args, (error, entries, tags, sounds) => {
             if (error) {
-                message.channel.send("No match found for '" + args + "', " + utilitiesModule.getRandomNameInsult());
+                message.channel.send(`No match found for '${args}', ${utilitiesModule.getRandomNameInsult()}`);
             }
             else {
-                text = "**Word**: " + entries[0].word +
-                    "\n\n**Definition**: *" + entries[0].definition + "*" +
-                    "\n\n**Example**: " + entries[0].example +
-                    "\n\n<" + entries[0].permalink + ">";
-                if (text.length >= 2000) message.channel.send("That urban dictionary article is too long!\n<" + entries[0].permalink + ">");
+                text = `**Word**: ${entries[0].word} (<${entries[0].permalink}>)
+                \n**Definition**: *${entries[0].definition}*
+                \n**Example**: ${entries[0].example}`;
+                
+                if (text.length >= 2000) message.channel.send(`The urban dictionary article for '${entries[0].word}' is too long!\n<${entries[0].permalink}>`);
                 else message.channel.send(text);
             }
         });
     }
+
     utilitiesModule.incrementUserDataValue(message.author, "urbanCalls", 1);
 }
 
