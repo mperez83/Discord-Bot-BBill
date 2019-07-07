aa123caf218ea9a7652a2e733f256910ea4aa0f3
Michael Perez <mperez980089@yahoo.com>
2019-07-07 15:11:15 -0700

    improved craft

--- javascript/commands/normal_commands/craft.js
+++ javascript/commands/normal_commands/craft.js
@@ -1,6 +1,7 @@
 const utilitiesModule = require('../../utilities');
 
 const patterns = [
+
     [
         [1, 1, 1],
         [1, 1, 1],
@@ -29,21 +30,149 @@ const patterns = [
         [1, 1, 1],
         [0, 2, 0],
         [0, 2, 0]
+    ],
+
+    [
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1]
+    ],
+
+    [
+        [1, 1, 1, 1, 1],
+        [1, 2, 2, 2, 1],
+        [1, 2, 0, 2, 1],
+        [1, 2, 2, 2, 1],
+        [1, 1, 1, 1, 1]
+    ],
+
+    [
+        [0, 0, 1, 0, 0],
+        [0, 1, 1, 1, 0],
+        [1, 1, 2, 1, 1],
+        [0, 1, 1, 1, 0],
+        [0, 0, 1, 0, 0]
+    ],
+
+    [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ],
+
+    [
+        [1, 2, 2],
+        [2, 2, 3],
+        [3, 3, 3]
+    ],
+
+    [
+        [0, 0, 0],
+        [0, 0, 1],
+        [0, 0, 2]
+    ],
+
+    [
+        [1, 1, 1],
+        [1, 2, 1],
+        [1, 1, 1]
+    ],
+
+    [
+        [0, 0, 0],
+        [0, 0, 0],
+        [1, 1, 1]
+    ],
+
+    //Beacon
+    [
+        [1, 1, 1],
+        [1, 2, 1],
+        [3, 3, 3]
+    ],
+
+    [
+        [0, 0, 3],
+        [0, 2, 0],
+        [1, 0, 0]
+    ],
+
+    //Bow
+    [
+        [0, 1, 2],
+        [1, 0, 2],
+        [0, 1, 2]
+    ],
+
+    //Fishing pole
+    [
+        [0, 0, 1],
+        [0, 1, 2],
+        [1, 0, 2]
+    ],
+
+    //Armor
+    [
+        [1, 1, 1],
+        [1, 0, 1],
+        [0, 0, 0]
+    ],
+
+    [
+        [1, 0, 1],
+        [1, 1, 1],
+        [1, 1, 1]
+    ],
+
+    [
+        [1, 1, 1],
+        [1, 0, 1],
+        [1, 0, 1]
+    ],
+
+    [
+        [0, 0, 0],
+        [1, 0, 1],
+        [1, 0, 1]
+    ],
+
+    [
+        [0, 3, 0],
+        [2, 1, 2],
+        [1, 1, 1]
     ]
+
 ];
 
 
 
 module.exports.run = async (bot, message, args) => {
 
-    //If the server has no emotes, yeet them out
+    if (message.channel.type == "dm") {
+        message.channel.send(`you can't craft without an audience, ${utilitiesModule.getRandomNameInsult(message.author)}`);
+        return;
+    }
+
     if (message.guild.emojis.size == 0) {
         message.channel.send(`This server doesn't have any emojis, ${utilitiesModule.getRandomNameInsult(message.author)}`);
         return;
     }
 
+
+
     let pattern = patterns[Math.floor(Math.random() * patterns.length)];
     let middleRow = Math.floor(pattern.length / 2);
+
+    let emptySpace = message.guild.emojis.find(x => x.name === "emptySpace");
+    if (!emptySpace) emptySpace = `⬛`;
 
     let ingredients = {};
     let recipeMsg = ``;
@@ -53,7 +182,7 @@ module.exports.run = async (bot, message, args) => {
         for (let col = 0; col < pattern[row].length; col++) {
 
             if (pattern[row][col] == 0) {
-                recipeMsg += `⬛`;
+                recipeMsg += `${emptySpace}`;
             }
             else {
                 let curIngredient = ingredients[pattern[row][col]];
