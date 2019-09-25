ffaa4a1611435413c9996db9daaaf0d142be8fad
Michael Perez <mperez980089@yahoo.com>
2019-09-24 18:05:56 -0700

    Improved sfw command

--- javascript/commands/normal_commands/sfw.js
+++ javascript/commands/normal_commands/sfw.js
@@ -4,11 +4,19 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    for (let i = 0; i < 3; i++) {
+    /*for (let i = 0; i < 3; i++) {
         let newEmbed = new Discord.RichEmbed();
         newEmbed.setImage(`https://cdn.discordapp.com/attachments/527341248214990850/584244981397454884/sfw_sasuke.jpg`);
         message.channel.send(newEmbed);
+    }*/
+
+    let msg = `<:vtwinkie1:595359362147680266>\n`;
+    for (let i = 0; i < 30; i++) {
+        msg += `<:vtwinkie2:595359360884932619>\n`;
     }
+    msg += `<:vtwinkie3:595359361283653632>`;
+
+    message.channel.send(msg);
 
 }
 
@@ -19,6 +27,7 @@ module.exports.help = {
     example: "!sfw",
     funFacts: [
         `If I recall correctly, this was the first command ever created. The concept of posting an image three times in a row was an easy enough task to act \
-        as my intro to Discord.js.`
+        as my intro to Discord.js.`,
+        `This command used to be three "sfw sasuke" images posted one after another. The new doge train is much more efficient, and tonally appropriate.`
     ]
 }
